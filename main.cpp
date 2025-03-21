12d2af177234ce622feb5ec490559ff4ac95fe6c
kakekalakalalala <physicalnoob@gmail.com>
2025-03-21 18:38:41 +0800

    coommiitt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-int printMenu(int &x){
+int printMenu(int &x, bool enabled){
     std::cout << "Welcome Message designed by your group\n";
     std::cout << "*** SMS Main Menu ***\n";
     std::cout << "[1] Load Starting Data\n";
@@ -13,18 +13,43 @@ int printMenu(int &x){
     std::cout << "*************\n";
     std::cout << "Option (1 - 6): ";
     std::cin >> x; //get user input
-    return x; // return user input
+    if(enabled || (x == 1)){
+        return x; // return user input only when data is loaded or is about to load data. R1.2  
+    }else if (!(enabled) && ((x >= 2) || (x <= 5))){ 
+        // show error if data is not loaded and user using function 2~5
+        std::cout << "Starting data is not yet loaded. Please load starting data first!\n";
+    }
 }
+
+class studentdata{
+    public:
+
+    private:
+    char name[30];
+    char id[7];
+    char major[30];
+    float gpa;
+    char subjectname[7];
+    int grade;
+    char grade[2];
+};
+
+
+
+
+
+
 
 main(){
     int response = 999; // set variable to hold user input;
-
+    bool boo = false;
     do{
-        printMenu(response);
+        printMenu(response, boo);
         switch (response)
         {
         case 1:
             std::cout << response << std::endl;
+            boo = true;
             break;
         case 2:
             std::cout << response << std::endl;
